cc2659140bd4f8641087f8d2ef76622646cbdd33
Raisul <raisulconnects@gmail.com>
2026-01-05 18:30:37 +0600

    Spinner working properly

--- src/utils/validateDomain.js
+++ src/utils/validateDomain.js
@@ -1,4 +1,5 @@
 export const isValidDomain = (value) => {
+  // this one ensures the domains are correct or not!
   const domainRegex = /^(?!-)(?:[a-zA-Z0-9-]{1,63}\.)+[a-zA-Z]{2,}$/;
   return domainRegex.test(value);
 };
